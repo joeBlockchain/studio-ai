374f2cc786d3b99787f79b3fdcbcb305f257eee5
joeBlockchain <94577719+joeBlockchain@users.noreply.github.com>
2024-11-12 23:15:37 -0800

    tailwind typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "tailwindcss";
+import tailwindAnimate from "tailwindcss-animate";
+import tailwindTypography from "@tailwindcss/typography";
 
 export default {
     darkMode: ["class"],
@@ -90,5 +92,8 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    tailwindAnimate,
+    tailwindTypography
+  ],
 } satisfies Config;
